211b7de2490d33ab1bd5f9b2e0e4ec7dc159bdcf
Katsumi Yamaoka <yamaoka@jpl.org>
1999-08-23 05:13:11 +0000

    New file separated from x-face-mule.el; import the splashing functions from BITMAP-DECOR 0.05.

---
+++ gnus-bitmap.el
@@ -0,0 +1,380 @@
+;; gnus-bitmap.el -- bitmap utilities for gnus.
+
+;; Copyright (C) 1999 Free Software Foundation, Inc.
+
+;; Author: KORIYAMA Naohiro <kory@ba2.so-net.ne.jp>
+;;         Katsumi Yamaoka  <yamaoka@jpl.org>
+;;         Tatsuya Ichikawa <ichikawa@erc.epson.com>
+;; Created: 1999/08/20
+;; Revised:
+;; Keywords: bitmap, x-face, splash, gnus
+
+;; This file is part of bitmap-mule.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; How to use:
+;;   If you would like to splash the startup screen with bitmap image,
+;;   put the following lines in your .emacs file before gnus is loaded.
+;;
+;;	(if window-system (require 'gnus-bitmap))
+;;
+;;   Or you can inhibit the use of bitmap image as below instead.
+;;
+;;	(setq gnus-bitmap-splash-image nil)
+
+;;; Code:
+
+(eval-when-compile
+  (require 'cl)
+  (require 'static))
+(require 'pcustom)
+(require 'alist)
+(require 'bitmap)
+(require 'x-face-mule)
+
+
+;;; Internal variables and the related functions.
+;;
+
+(eval-when-compile
+  (defmacro gnus-bitmap-xbm-to-bitmap (file)
+    (` (let* ((cmp (bitmap-decode-xbm (bitmap-read-xbm-file (, file))))
+	      (len (length cmp))
+	      (bitmap (bitmap-compose (aref cmp 0)))
+	      (i 1))
+	 (while (< i len)
+	   (setq bitmap (concat bitmap "\n" (bitmap-compose (aref cmp i)))
+		 i (1+ i)))
+	 bitmap)))
+  )
+
+(eval-when-compile
+  (defmacro gnus-bitmap-splash-image-internal ()
+    (if (file-exists-p "gnus.xbm")
+	(progn
+	  (message "  ++ composing internal image for splashing...")
+	  (gnus-bitmap-xbm-to-bitmap "gnus.xbm"))
+      (byte-compile-warn "Warning: file \"gnus.xbm\" not found.")
+      nil))
+
+  (defmacro gnus-bitmap-modeline-image-internal ()
+    (if (file-exists-p "gnus-pointer.xbm")
+	(progn
+	  (message "  ++ composing internal image for modeline identifier...")
+	  (gnus-bitmap-xbm-to-bitmap "gnus-pointer.xbm"))
+      (byte-compile-warn "Warning: file \"gnus-pointer.xbm\" not found.")
+      nil))
+  )
+
+(defconst gnus-bitmap-splash-image-internal
+  (gnus-bitmap-splash-image-internal))
+
+(defconst gnus-bitmap-modeline-image-internal
+  (gnus-bitmap-modeline-image-internal))
+
+(defvar gnus-bitmap-splash-image-data nil)
+
+(defvar gnus-bitmap-modeline-identifier nil)
+
+(eval-when-compile
+  (makunbound 'gnus-bitmap-broken-facility-check)
+  (defcustom gnus-bitmap-broken-facility-check t
+    "The value t means `:set' does not work at the loading time.  For instance
+it will be occurred under Mule 2.3 based on Emacs 19.34 using custom-1.9962."
+    :set (function (lambda (symbol value) (set-default symbol nil)))))
+
+(static-if gnus-bitmap-broken-facility-check
+    (progn
+      (defun gnus-bitmap-set-splash-image-data (arg)
+	(setq gnus-bitmap-splash-image-data
+	      (cond ((eq 'internal arg)
+		     gnus-bitmap-splash-image-internal)
+		    ((and (stringp arg) (file-exists-p arg))
+		     (gnus-bitmap-xbm-to-bitmap arg)))))
+      (defun gnus-bitmap-set-modeline-image-data (arg)
+	(setq gnus-bitmap-modeline-identifier
+	      (cond ((eq 'internal arg)
+		     gnus-bitmap-modeline-image-internal)
+		    ((and (stringp arg) (file-exists-p arg))
+		     (gnus-bitmap-xbm-to-bitmap arg)))))
+      )
+  (defmacro gnus-bitmap-set-splash-image-data (arg)
+    (` (setq gnus-bitmap-splash-image-data
+	     (cond ((eq 'internal (, arg))
+		    gnus-bitmap-splash-image-internal)
+		   ((and (stringp (, arg)) (file-exists-p (, arg)))
+		    (gnus-bitmap-xbm-to-bitmap (, arg)))))))
+  (defmacro gnus-bitmap-set-modeline-image-data (arg)
+    (` (setq gnus-bitmap-modeline-identifier
+	     (cond ((eq 'internal (, arg))
+		    gnus-bitmap-modeline-image-internal)
+		   ((and (stringp (, arg)) (file-exists-p (, arg)))
+		    (gnus-bitmap-xbm-to-bitmap (, arg)))))))
+  )
+
+
+;;; Options.
+;;
+
+(defcustom x-face-mule-gnus-force-decode-headers nil
+  "If non-nil, display X-Face even if `gnus-show-mime' is nil."
+  :type 'boolean
+  :group 'x-face-mule)
+
+(defcustom gnus-bitmap-splash-image 'internal
+  "If it is the symbol `internal', use the internal image for splashing
+the startup screen.  It can also be a name of XBM file.  If it is nil,
+don't use bitmap image."
+  :type (list 'radio
+	      (list 'const :tag
+		    (concat "Internal image\n"
+			    gnus-bitmap-splash-image-internal)
+		    'internal)
+	      '(file  :tag "XBM file")
+	      '(const :tag "Off" nil))
+  :set (function
+	(lambda (symbol value)
+	  (gnus-bitmap-set-splash-image-data value)
+	  (set-default symbol value)))
+  :group 'gnus-start)
+
+(defcustom gnus-bitmap-modeline-image 'internal
+  "If it is the symbol `internal', use the internal image for displaying
+a nifty image in the modeline.  It can also be a name of XBM file.  If it
+is nil or the value of `emacs-major-version' is less than 20, don't use
+bitmap image."
+  :type (list 'radio
+	      (list 'const :tag
+		    (concat "Internal image ["
+			    gnus-bitmap-modeline-image-internal "]")
+		    'internal)
+	      '(file  :tag "XBM file")
+	      '(const :tag "Off" nil))
+  :set (function
+	(lambda (symbol value)
+	  (gnus-bitmap-set-modeline-image-data value)
+	  (set-default symbol value)))
+  :group 'gnus-start)
+
+(static-when gnus-bitmap-broken-facility-check
+  (gnus-bitmap-set-splash-image-data gnus-bitmap-splash-image)
+  (gnus-bitmap-set-modeline-image-data gnus-bitmap-modeline-image))
+
+
+;;; Avoid byte compile warnings.
+;;
+
+(defvar gnus-article-display-hook)
+(defvar gnus-article-x-face-command)
+(defvar gnus-ignored-headers)
+(defvar gnus-mule-bitmap-image-file)
+(defvar gnus-simple-splash)
+(defvar gnus-strict-mime)
+(defvar gnus-treat-display-xface)
+(defvar gnus-treat-hide-headers)
+(defvar gnus-version)
+(defvar gnus-visible-headers)
+(defvar last)
+(eval-when-compile
+  (fset 'gnus-bitmap-group-startup-message 'ignore)
+  (autoload 'gnus-indent-rigidly "gnus")
+  (autoload 'gnus-summary-select-article "gnus-sum"))
+
+
+;;; X-Face functions.
+;;
+
+(defun x-face-mule-gnus-article-display-x-face (&rest args)
+  "Decode and show X-Face.  The buffer is expected to be narrowed to just the
+headers of the article.  If `gnus-article-x-face-command' is set to the
+symbol of this function, gnus will call it for each X-Face fields.  So we
+set the bound variable `last' to T in order to make good use of time."
+  (let ((x-face-mule-highlight-x-face-style
+	 (if (and (eq x-face-mule-highlight-x-face-style 'xmas)
+		  (memq this-command
+			'(gnus-summary-toggle-mime)))
+	     'default
+	   x-face-mule-highlight-x-face-style))
+	x-face-mule-preserve-original-from-field)
+    (x-face-mule-x-face-decode-message-header-1)
+    (x-face-mule-highlight-header)
+    (setq last t)))
+
+(defun x-face-mule-gnus-highlight-headers-if-no-mime ()
+  "Decode and show X-Face even if `gnus-show-mime' is nil.  It is
+controlled by the value of `x-face-mule-gnus-force-decode-headers'."
+  (when (and x-face-mule-gnus-force-decode-headers
+	     window-system
+	     (eq major-mode 'gnus-article-mode)
+	     (memq x-face-mule-highlight-x-face-position '(from x-face)))
+    (x-face-mule-x-face-decode-message-header)))
+
+
+;;; Splash the startup screen.
+;;
+
+(defun gnus-bitmap-sit-for-0 ()
+  (remove-hook 'gnus-startup-hook 'gnus-bitmap-sit-for-0)
+  (sit-for 0))
+
+(defun gnus-bitmap-startup-message (&optional x y)
+  "Insert startup message in current buffer."
+  (if (null gnus-bitmap-splash-image-data)
+      (gnus-bitmap-group-startup-message x y)
+    (erase-buffer)
+    (insert gnus-version "\n")
+    (let ((fill-column (1- (window-width))))
+      (center-region (point-min) (point-max)))
+    (let ((start (point))
+	  width)
+      ;; Insert an image.
+      (insert gnus-bitmap-splash-image-data)
+      (setq width (current-column))
+      (insert "\n")
+      ;; And then hack it.
+      (gnus-indent-rigidly start (point)
+			   (/ (max (- (window-width) (or x (1- width))) 0) 2))
+      (goto-char start)
+      (insert (make-string
+	       (/ (max 0
+		       (- (window-height) (count-lines start (point-max)))) 2)
+	       ?\n)))
+    ;; Fontify some.
+    (put-text-property (point-min) (point-max) 'face 'gnus-splash-face)
+    (goto-char (point-min))
+    (setq mode-line-buffer-identification gnus-version)
+    (setq gnus-simple-splash nil)
+    (set-buffer-modified-p t)
+    ;; Redisplay after `gnus-startup-hook' is evaluated.
+    (add-hook 'gnus-startup-hook 'gnus-bitmap-sit-for-0 'append)))
+
+
+;;; Modeline identification.
+;;
+
+(defun gnus-bitmap-mode-line-buffer-identification (line)
+  (let ((l (car line)))
+    (if (and (stringp l) gnus-bitmap-modeline-identifier)
+	(list gnus-bitmap-modeline-identifier
+	      (if (string-match "^Gnus: *" l)
+		  (substring l (match-end 0))
+		l))
+      line)))
+
+
+;;; Setup.
+;;
+
+(set-alist 'x-face-mule-highlight-x-face-refresh-method-alist
+	   'gnus-summary-mode
+	   (function (lambda () (gnus-summary-select-article nil t))))
+
+(add-hook 'gnus-exit-gnus-hook 'x-face-mule-save-cache-file)
+
+(defun gnus-bitmap-redefine ()
+  "Redifine variables and functions for the use of bitmap-mule."
+  (require 'gnus-art)
+  (setq gnus-strict-mime nil)
+
+  ;; Modify `gnus-visible-headers' and `gnus-ignored-headers'.
+  (cond ((stringp gnus-visible-headers)
+	 (let ((xf (string-match gnus-visible-headers "X-Face:"))
+	       (xfi (string-match gnus-visible-headers "X-Face-Img:")))
+	   (cond ((not (or xf xfi))
+		  (setq gnus-visible-headers
+			(format "\\(%s\\)\\|^X-Face:\\|^X-Face-Img:"
+				gnus-visible-headers)))
+		 ((not xf)
+		  (setq gnus-visible-headers
+			(format "\\(%s\\)\\|^X-Face:"
+				gnus-visible-headers)))
+		 ((not xfi)
+		  (setq gnus-visible-headers
+			(format "\\(%s\\)\\|^X-Face-Img:"
+				gnus-visible-headers))))))
+	((and gnus-visible-headers (listp gnus-visible-headers))
+	 (unless (member "^X-Face:" gnus-visible-headers)
+	   (setq gnus-visible-headers
+		 (append gnus-visible-headers '("^X-Face:"))))
+	 (unless (member "^X-Face-Img:" gnus-visible-headers)
+	   (setq gnus-visible-headers
+		 (append gnus-visible-headers '("^X-Face-Img:")))))
+	((stringp gnus-ignored-headers)
+	 (let ((regexp (format  "\\\\|%s\\|%s\\\\|%s"
+				"\\^?X-Face\\(-Img\\)?:"
+				"\\^?X-Face\\(-Img\\)?:"
+				"\\^?X-Face\\(-Img\\)?:")))
+	   (while (string-match regexp gnus-ignored-headers)
+	     (setq gnus-ignored-headers
+		   (concat (substring gnus-ignored-headers 0
+				      (match-beginning 0))
+			   (substring gnus-ignored-headers (match-end 0)))))))
+	((and gnus-ignored-headers (listp gnus-ignored-headers))
+	 (when (member "^X-Face:" gnus-ignored-headers)
+	   (setq gnus-ignored-headers
+		 (delete "^X-Face:" gnus-ignored-headers)))
+	 (when (member "^X-Face-Img:" gnus-ignored-headers)
+	   (setq gnus-ignored-headers
+		 (delete "^X-Face-Img:" gnus-ignored-headers)))
+	 ))
+
+  ;; Pterodactyl Gnus, T-gnus 6.10.064 or later.
+  (setq gnus-treat-display-xface
+	(if (and (featurep 'running-pterodactyl-gnus-0_73-or-later); T-gnus
+		 (not x-face-mule-gnus-force-decode-headers))
+	    '(and mime head)
+	  'head)
+	gnus-treat-hide-headers 'head
+	gnus-article-x-face-command 'x-face-mule-gnus-article-display-x-face)
+
+  ;; Advice the hiding function.
+  (defadvice article-hide-headers (after x-face-mule-highlight-header
+					 activate)
+    "Highlight X-Face field(s) after `article-hide-headers' is done."
+    (x-face-mule-highlight-header))
+
+  ;; Gnus 5.7, Semi-gnus 6.10, Nana-gnus 6.13 or earlier.
+  (when (and (boundp 'gnus-article-display-hook)
+	     (or (memq 'gnus-article-hide-headers-if-wanted
+		       gnus-article-display-hook)
+		 (memq 'gnus-article-maybe-highlight
+		       gnus-article-display-hook)))
+    (remove-hook 'gnus-article-display-hook 'gnus-article-display-x-face)
+    (add-hook 'gnus-article-display-hook
+	      'x-face-mule-gnus-highlight-headers-if-no-mime 'append)
+    )
+
+  ;; Redifine the splash function.
+  (unless (fboundp 'gnus-bitmap-group-startup-message)
+    (fset 'gnus-bitmap-group-startup-message
+	  (symbol-function 'gnus-group-startup-message)))
+  (fset 'gnus-group-startup-message 'gnus-bitmap-startup-message)
+  (when (boundp 'gnus-mule-bitmap-image-file)
+    (setq gnus-mule-bitmap-image-file nil))
+
+  (when (>= emacs-major-version 20)
+    ;; Redifine the modeline identify function.
+    (fset 'gnus-mode-line-buffer-identification
+	  'gnus-bitmap-mode-line-buffer-identification))
+  )
+
+
+(provide 'gnus-bitmap)
+
+;; gnus-bitmap.el ends here
