2d19d13d99d1022f41367b6f1efff1eeea25e0bc
Katsumi Yamaoka <yamaoka@jpl.org>
2000-03-29 04:04:42 +0000

    (bitmap-lprogress-display): Show percentage. (bitmap-string-to-special-symbols): New function. (bitmap-special-symbol-alist): New variable.

--- bm-utils.el
+++ bm-utils.el
@@ -4,6 +4,7 @@
 
 ;; Author: Katsumi Yamaoka <yamaoka@jpl.org>
 ;; Created: 2000/03/28
+;; Revised: 2000/03/29
 ;; Keywords: bitmap, lprogress-display
 
 ;; This file is part of bitmap-mule.
@@ -30,6 +31,19 @@
 (defvar bitmap-lprogress-display-textual nil
   "*If it is non-nil, progress display will be textual.")
 
+(defvar bitmap-special-symbol-alist
+  (list (cons ?- (bitmap-compose "000000000000003C7800000000000000"))
+	(cons ?0 (bitmap-compose "007CBAC6C6C6C2800286C6C6C6BA7C00"))
+	(cons ?1 (bitmap-compose "00000206060602000206060606020000"))
+	(cons ?2 (bitmap-compose "007C3A060606023C7880C0C0C0B87C00"))
+	(cons ?3 (bitmap-compose "007C3A060606023C7A060606063A7C00"))
+	(cons ?4 (bitmap-compose "000082C6C6C6C2BC7A06060606020000"))
+	(cons ?5 (bitmap-compose "007CB8C0C0C0C0BC7A060606063A7C00"))
+	(cons ?6 (bitmap-compose "007CB8C0C0C0C0BC7A86C6C6C6BA7C00"))
+	(cons ?7 (bitmap-compose "007C3A06060602000206060606020000"))
+	(cons ?8 (bitmap-compose "007CBAC6C6C6C2BC7A86C6C6C6BA7C00"))
+	(cons ?9 (bitmap-compose "007CBAC6C6C6C2BC7A060606063A7C00"))))
+
 (defvar bitmap-lprogress-data
   (mapcar 'bitmap-compose '("55AA552A152A152A152A152A15AA55AA"
 			    "55AA550A050A050A050A050A05AA55AA"
@@ -40,6 +54,13 @@
 	(string-to-char (bitmap-compose "55AA5500000000000000000000AA55AA"))
 	(string-to-char (bitmap-compose "55AA55AA55AA55AA55AA55AA55AA55AA"))
 	(bitmap-compose "40A040A040A040A040A040A040A040A0")))
+
+(defun bitmap-string-to-special-symbols (string)
+  "Convert STRING to a special symbols."
+  (mapconcat (function (lambda (char)
+			 (or (cdr (assq char bitmap-special-symbol-alist))
+			     (char-to-string char))))
+	     string ""))
 
 (defun bitmap-lprogress-display (label fmt &optional value &rest args)
   "Print a progress gauge and message in the echo area.
@@ -55,22 +76,33 @@ the same as to `format'.  [XEmacs 21.2.32 emulating function]"
 	  (message "%s%s%s"
 		   msg (make-string (/ value 5) ?.)
 		   (if (eq 100 value) "done" ""))
-	(let ((msgmax (- (window-width (minibuffer-window)) 27)))
-	  (cond ((< msgmax 3)
-		 (setq msg ""))
+	(let ((msgmax (- (window-width (minibuffer-window)) 35)))
+	  (cond ((< msgmax 0)
+		 (setq msg (make-string (max 0 (+ 3 msgmax)) ? )))
 		((> (string-width msg) msgmax)
-		 (setq msg (concat (truncate-string msg (- msgmax 3))
-				   "...")))))
-	(message "%s%s%s%s%s%s"
-		 msg
-		 (car bitmap-lprogress-backgrounds)
-		 (make-string (/ value 4) (nth 1 bitmap-lprogress-backgrounds))
-		 (if (zerop (% value 4))
-		     ""
-		   (nth (1- (% value 4)) bitmap-lprogress-data))
-		 (make-string (- 25 (/ (+ 3 value) 4))
-			      (nth 2 bitmap-lprogress-backgrounds))
-		 (nth 3 bitmap-lprogress-backgrounds))))))
+		 (setq msg (truncate-string msg msgmax)
+		       msg (concat
+			    msg "..."
+			    (make-string (- msgmax (string-width msg)) ? ))))
+		(t
+		 (setq msg (concat
+			    msg
+			    (make-string (+ 3 (- msgmax (string-width msg)))
+					 ? ))))))
+	(let ((cursor-in-echo-area t))
+	  (message "%s%s%s%s%s%s%s"
+		   msg
+		   (car bitmap-lprogress-backgrounds)
+		   (make-string (/ value 4)
+				(nth 1 bitmap-lprogress-backgrounds))
+		   (if (zerop (% value 4))
+		       ""
+		     (nth (1- (% value 4)) bitmap-lprogress-data))
+		   (make-string (- 25 (/ (+ 3 value) 4))
+				(nth 2 bitmap-lprogress-backgrounds))
+		   (nth 3 bitmap-lprogress-backgrounds)
+		   (bitmap-string-to-special-symbols
+		    (format "%3d%%" value))))))))
 
 (defalias 'lprogress-display 'bitmap-lprogress-display)
 
@@ -82,7 +114,7 @@ the same as to `format'.  [XEmacs 21.2.32 emulating function]"
 ;;    (while (< n 100)
 ;;      (if textual
 ;;	  (lprogress-display nil "Processing" n)
-;;	(lprogress-display nil "Processing...% 3d%%" n n))
+;;	(lprogress-display nil "Processing..." n))
 ;;      (sleep-for 0.1)
 ;;      (setq n (1+ n)))
 ;;    (if textual
